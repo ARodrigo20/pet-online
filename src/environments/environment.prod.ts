d97ba62d3e2568a38606912ddbb87746b7b424e9
DESKTOP-75F08NR\SH4-L <alejandro.flores@pangolabs.pe>
2022-01-18 06:16:32 -0500

    primer

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  REST_API_SERVER: 'https://generic-nodejs.herokuapp.com/api/',
+
+  //For facebook API
+  apiUrl: 'http://localhost:4000',
+  facebookAppId: '314930319788683'
+};
